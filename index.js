1f5b8b1bfabd57f51d207a38a24ae5b16b5a6226
Akki <aakash.sharma413@gmail.com>
2025-03-17 18:18:18 +0530

    multiple url parameter and invalid route

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 import express from 'express';
-import router from './route.js';
 const app = express();
 const PORT = 3000;
 
@@ -7,32 +6,18 @@ app.get("/", (req, res) => {
     res.send("Hello, express")
 });
 
-app.use('/user', router);
-
-app.use(express.json());
-
-app.post('/users', (req, res) => {
-    const { name, email } = req.body;
-    res.send({
-        message: `User ${name} with email ${email} is created successfully!`
-    })
-})
-
-app.put('/users/:id', (req, res) => {
-    const userId = req.params.id;
-    const { name, email } = req.body;
+// handle multiple url parameters
+app.get('/products/:product/:id([0-9]{5})', (req, res) => {
+    const { product, id } = req.params
     res.json({
-        message: `User ${userId} is updated with ${name}, ${email}`
+        product, id
     })
 })
 
-app.delete('/users/:id', (req, res) => {
-    const userId = req.params.id
-    res.json({
-        message: `User with id ${userId} is deleted successfully!`
-    })
+// handle invalid route
+app.get('*', (req, res) => {
+    res.send('Sorry, this is a invalid url!')
 })
-
 
 app.listen(PORT, () => {
     console.log(`Server is listening on port http://localhost${PORT}`)
