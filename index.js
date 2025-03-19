294879745738d888a913ab4e6498a6b45afa85f3
Akki <aakash.sharma413@gmail.com>
2025-03-19 18:15:27 +0530

    handled form data with multer

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
-import express from 'express';
+import express from "express";
+import multer from "multer";
+import { storage } from "./config/multer.js";
+
 const app = express();
+const upload = multer({ storage, limits: { fileSize: 1024000 } });
 const PORT = 3000;
 
-app.use('/public', express.static('public'))
-app.use('/images', express.static('images'))
+// app.use(express.urlencoded({extended: true}))
+app.use(upload.single("image"));
 
 app.get("/", (req, res) => {
-    res.send('Hello Express')
+  res.send("Hello Express");
 });
 
+app.post("/form", (req, res) => {
+  console.log(req.body);
+  console.log(req.file);
+  res.send("Form received");
+});
 
 app.listen(PORT, () => {
-    console.log(`Server is listening on port http://localhost${PORT}`)
-})
+  console.log(`Server is listening on port http://localhost${PORT}`);
+});
