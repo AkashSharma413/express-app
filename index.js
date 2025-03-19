ae7dd7c5338f626ccf011ecf09e82d0ac0f05073
Akki <aakash.sharma413@gmail.com>
2025-03-19 14:19:17 +0530

    ejs template in express

--- index.js
+++ index.js
@@ -2,18 +2,14 @@ import express from 'express';
 const app = express();
 const PORT = 3000;
 
+// set EJS as the view engine
+app.set('view engine', 'ejs')
+
 app.get("/", (req, res) => {
-    res.send("Hello, express")
+    const userName = 'Elon Musk'    
+    res.render('index', {userName})
 });
 
-app.get('/error', (req, res) => {
-    throw new Error('This is test error')
-})
-
-app.use((err, req, res, next) => {
-    console.error(err.message)
-    res.send('Internal Server Error')
-})
 
 app.listen(PORT, () => {
     console.log(`Server is listening on port http://localhost${PORT}`)
