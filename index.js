46428658e442cd65bed4cd3d8e5635d9b0ef73d4
Akki <aakash.sharma413@gmail.com>
2025-03-19 13:31:47 +0530

    sequence of middleware execution

--- index.js
+++ index.js
@@ -2,22 +2,19 @@ import express from 'express';
 const app = express();
 const PORT = 3000;
 
-app.get("/", (req, res) => {
-    res.send("Hello, express")
-});
+app.use('/', (req, res, next) => {
+    console.log("Start")
+    next()
 
-// handle multiple url parameters
-app.get('/products/:product/:id([0-9]{5})', (req, res) => {
-    const { product, id } = req.params
-    res.json({
-        product, id
+    res.on('finish', () => {
+        console.log("End")
     })
 })
 
-// handle invalid route
-app.get('*', (req, res) => {
-    res.send('Sorry, this is a invalid url!')
-})
+app.get("/", (req, res) => {
+    console.log("Middle")
+    res.send("Hello, express")
+});
 
 app.listen(PORT, () => {
     console.log(`Server is listening on port http://localhost${PORT}`)
