b1b4d721e9169225391e254a2039072f06396945
Akki <aakash.sharma413@gmail.com>
2025-03-31 14:53:15 +0530

    implemented cookies functionalities

--- index.js
+++ index.js
@@ -1,49 +1,26 @@
 import express from "express";
-import { connectDb } from "./config/db.js";
-import { Person } from "./models/Person.js";
+import cookieParser from "cookie-parser";
 
 const app = express();
 const PORT = 3000;
-await connectDb();
+
+app.use(cookieParser());
 
 app.get("/", (req, res) => {
+  // res.cookie("name", "express-app", { maxAge: 10000});
+  res.cookie("name", "express-app");
   res.send("Hello Express");
 });
 
-app.use(express.json());
+app.get("/fetch", (req, res) => {
+  console.log(req.cookies);
+  res.send("API called")
+})
 
-// Save/Add user to database
-app.post("/person", async (req, res) => {
-  try {
-    const { name, email, age } = req.body;
-    const newPerson = new Person({
-      name,
-      age,
-      email,
-    });
-    await newPerson.save();
-    res.send("Person added");
-  } catch (error) {
-    res.send(error.message)
-  }
-});
-
-// Update user in database
-app.put("/person", async (req, res) => {
-  const { _id } = req.body;
-  const personData = await Person.findByIdAndUpdate(_id, {
-    name: "Akash Sharma",
-  });
-  console.log(personData);
-  res.send("Person updated");
-});
-
-// Delete user from database
-app.delete("/person/:id", async (req, res) => {
-  const { id } = req.params;
-  await Person.findByIdAndDelete(id);
-  res.send("User deleted");
-});
+app.get("/remove-cookie", (req, res) => {
+  res.clearCookie("name")
+  res.send("Cookie cleared")  
+})
 
 app.listen(PORT, () => {
   console.log(`Server is listening on port http://localhost${PORT}`);
