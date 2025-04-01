2a91843258833491a5b9da8751a50d70906a10a6
Akki <aakash.sharma413@gmail.com>
2025-04-01 14:03:07 +0530

    error handling at route level and global level using middleware

--- index.js
+++ index.js
@@ -5,38 +5,39 @@ const PORT = 3000;
 
 app.use(express.json());
 
+process.on("uncaughtException", (err) => {
+  console.log(err);
+  process.exit(1);
+});
+
+process.on("unhandledRejection", (reason, Promise) => {
+  console.log(reason);
+});
+
 app.get("/", (req, res) => {
   res.send("Hello Express");
 });
 
-// Get all products
-app.get("/api/products", (req, res) => {
-  const products = [
-    { id: 1, name: "Laptop", price: 10000 },
-    { id: 2, name: "Mobile", price: 5000 },
-  ];
-  res.status(200).json({ products });
-});
-
-// Get a single product
-app.get("/api/products/:id", (req, res) => {
-  const products = [
-    { id: 1, name: "Laptop", price: 10000 },
-    { id: 2, name: "Mobile", price: 5000 },
-  ];
-  const product = products.find((p) => p.id === Number(req.params.id));
-  if (!product) {
-    return res.status(404).json({ message: "Product not found" });
+app.get("/sync-error", (req, res, next) => {
+  try {
+    throw new Error("Something went wrong!");
+  } catch (error) {
+    next(error);
   }
-  res.status(200).json(product);
 });
 
-// Add/create a product
-app.post("/api/products", (req, res) => {
-  const newProduct = req.body;
-  newProduct.id = Date.now();
+app.get("/async-error", async (req, res, next) => {
+  try {
+    await Promise.reject(new Error("Async error occured"));
+  } catch (error) {
+    next(error);
+  }
+});
 
-  res.status(201).json(newProduct);
+app.use((err, req, res, next) => {
+  console.error(err.message);
+  console.log(err.stack);
+  res.status(500).json({ message: err.message });
 });
 
 app.listen(PORT, () => {
